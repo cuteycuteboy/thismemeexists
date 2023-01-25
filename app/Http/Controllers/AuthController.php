1fe062dd2d00c8f5a4b4c0fee46dbe871c0a3b0d
Maxim <cuteycuteboy@gmail.com>
2023-01-25 23:48:44 +0300

    add new methods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function showLoginForm()
+    {
+        return view("auth.login");
+    }
+
+    public function showRegisterForm()
+    {
+        return view("auth.register");
+    }
 }
