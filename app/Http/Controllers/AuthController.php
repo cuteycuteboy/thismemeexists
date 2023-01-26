83cbc18c9e6582ddb5f548685e1ff728c750d1a2
Maxim <cuteycuteboy@gmail.com>
2023-01-26 22:15:22 +0300

    add register,login,logout methods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,9 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AuthController extends Controller
 {
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            "email" => ["required", "email", "string"],
+            "password" => ["required"]
+        ]);
+
+        if(auth("web")->attempt($data)) {
+            return redirect(route("home"));
+        }
+
+        return redirect(route("login"))->withErrors(["email" => "Пользователь не найден, либо данные введены не правильно"]);
+    }
+
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            "name" => ["required", "string", "min:1","max:32"],
+            "email" => ["required", "email", "string", "unique:users,email", "min:8","max:32"],
+            "password" => ["required", "confirmed","min:8","max:32"]
+        ]);
+
+        $user = User::create([
+            "name" => $data["name"],
+            "email" => $data["email"],
+            "password" => bcrypt($data["password"])
+        ]);
+
+        if($user) {
+            auth("web")->login($user);
+        }
+
+        return redirect(route("home"));
+    }
+
+    public function logout()
+    {
+        auth("web")->logout();
+
+        return redirect(route("home"));
+    }
+
     public function showLoginForm()
     {
         return view("auth.login");
