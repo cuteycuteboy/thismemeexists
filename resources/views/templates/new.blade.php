fe7ec8c53daa23c9f6635523460e20dc4c8caf1b
Maxim <cuteycuteboy@gmail.com>
2023-02-08 21:38:33 +0300

    add enctype

--- resources/views/templates/new.blade.php
+++ resources/views/templates/new.blade.php
@@ -4,11 +4,11 @@
 
 @section('content')
     @include('partials.header')
-    <form method="POST" action="{{ route("new_template") }}">
+    <form method="POST" enctype="multipart/form-data" action="{{ route("new_template") }}">
         @csrf
         <div class="container p-3">
             <div class="fs-2  mb-4">Добавить свой шаблон</div>
-            <input class="form-control form-control-lg mb-3" type="file" id="formFile">
+            <input name="template" class="form-control form-control-lg mb-3" type="file" id="File">
             <button type="submit" class="btn btn-primary btn-lg">Подтвердить</button>
         </div>
     </form>
