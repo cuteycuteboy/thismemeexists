29ebffb567364d6245ae715abd386e93c3f84186
cuteycuteboy <cuteycuteboy@gmail.com>
2023-03-06 14:53:47 +0300

    add meme model

---
+++ database/migrations/2023_03_05_180337_create_memes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable();
+            $table->foreignId('template_id');
+            $table->string('image_path');
+            $table->string('thumbnail_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memes');
+    }
+};
