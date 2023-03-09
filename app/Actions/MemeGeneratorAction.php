6cdaf4b83bcf31d3c57295098366ac8972428d53
cuteycuteboy <cuteycuteboy@gmail.com>
2023-03-09 23:04:20 +0300

    meme generator v2

--- app/Actions/MemeGeneratorAction.php
+++ app/Actions/MemeGeneratorAction.php
@@ -17,6 +17,8 @@ class MemeGeneratorAction{
 	private $im;
 	private $width;
 	private $height;
+	private $textMaxWidth;
+	private $textMaxHeight;
 
 	public function __invoke($id,$topText,$bottomText)
 	{
@@ -30,7 +32,7 @@ class MemeGeneratorAction{
 		$this->setBottomText($bottomText);
 
 		$this->processImg();
-
+		//dd();
 		return $this->im; 
     }
 
@@ -59,11 +61,6 @@ class MemeGeneratorAction{
 	{
 		$this->bottomText = mb_strtoupper($txt);
 	}
-
-	private function checkTextWidthExceedImage($imgWidth, $textWidth)
-	{
-		return ($imgWidth < $textWidth + 20);
-	}
 	
 	private function getFontPlacementCoordinates($text, $fontSize)
 	{
@@ -84,112 +81,82 @@ class MemeGeneratorAction{
 		return imagettfbbox($fontSize, 0, $this->font, $text);
 	}
 
-	private function placeTextOnImage($fontSize, $Xlocation, $Textheight,$text)
+	private function placeTextOnImage($fontSize, $multipleLinesText, $type)
 	{
-		$this->im->text($text,$Xlocation,$Textheight,function($font) use ($fontSize) {
-			$font->file($this->font);
-			$font->size($fontSize);
-			$font->color('#FFFFFF');
-			$font->align('center');
-			$font->valign('top');
-		});
+		$textCoordinateX = floor($this->width / 2);
+		if ($type == 'top'){
+			$textCoordinateY = round($this->height/32);
+		}else{
+			$textCoordinateY = $this->height - $multipleLinesText[1] - round($this->height/32);
+		}
+		foreach ($multipleLinesText[0] as $line){
+			$this->im->text($line[0],$textCoordinateX,$textCoordinateY, function($font) use ($fontSize) {
+				$font->file($this->font);
+				$font->size($fontSize);
+				$font->color('#FFFFFF');
+				$font->align('center');
+				$font->valign('top');
+			});
+			$textCoordinateY += $line[1];
+		}
 	}
 	
-	private function workOnImage($text,$fontSize,$type)
+	public function workOnImage($text, $type)
 	{
-		if ($type == "top") {
-			$textCoordinateY = 35;
-		}
-      	else {
-        	$textCoordinateY = $this->height - 20;
-		}
 
-		$textCoordinateX = floor($this->width / 2);
+		$maxFontSize = round(($this->height / 8) * 0.75);
+		$minFontSize = 1;
 
-		while (True) {
-		  	// //get coordinate for the text
-		  	$coords = $this->getFontPlacementCoordinates($text, $fontSize);
-	
-		  	//check if the text does not exceed image width if yes then repeat with size = size - 1
-		  	if ($this->checkTextWidthExceedImage($this->width, $coords[2] - $coords[0])) {
-				//if top text take it up as font size decreases, if bottom text take it down as font size decreases
-				$textCoordinateY += ($type == "top") ? - 1 : 1;
-	
-				if ($fontSize == 10) {
-			  		//if text size is reached to lower limit and still it is exceeding image width start breaking into lines
-			  		if ($type == "top") {
-						$this->topText = $this->returnMultipleLinesText($text, $type, 16);
-						$text = $this->topText;
-						return;
-			  		}
-			  		else {
-						$this->bottomText = $this->returnMultipleLinesText($text, $type, $this->height - 20);
-						$text = $this->bottomText;
-						return;
-			  		}
-				}
-				else{
-					$fontSize -=1;
-				}
-		  	}
-		  	else{
-				break;
-		  	}
-		}
-	
-		if ($type == "top"){
-		  	$this->placeTextOnImage($fontSize, $textCoordinateX, $textCoordinateY, $this->topText);
-		}
-		else{
-		  	$this->placeTextOnImage($fontSize, $textCoordinateX, $textCoordinateY, $this->bottomText);
-		}
-	}
+		while ($maxFontSize > $minFontSize + 1){
+			$fontSize = floor(($maxFontSize+$minFontSize) / 2);
+			$textHeight =  $this->returnMultipleLinesText($text, $fontSize)[1];
 
-	private function returnMultipleLinesText($text, $type, $textHeight) {
-		//breaks the whole sentence into multiple lines according to the width of the image.
-		//break sentence into an array of words by using the spaces as params
-		$brokenText = explode(" ", $text);
-		$finalOutput = "";
-	
-		if ($type != "top"){
-		  	$textHeight = $this->height - ((count($brokenText) / 2) * 3);
-		}
-		$textCoordinateX = floor($this->width / 2);
-	
-		for ($i = 0; $i < count($brokenText); $i++) {
-			$temp = $finalOutput;
-			$finalOutput.= $brokenText[$i] . " ";
-			// this will help us to keep the last word in hand if this word is the cause of text exceeding the image size.			
-			// We will be using this to append in next line.
-			//check if word is too long i.e wider than image width
-			//get the sentence(appended till now) placement coordinates
-			$dimensions = $this->getFontPlacementCoordinates($finalOutput, 10);
-		
-			//check if the sentence (till now) is exceeding the image with new word appended
-			if ($this->checkTextWidthExceedImage($this->width, $dimensions[2] - $dimensions[0])) { //yes it is then
-				// append the previous sentence not with the new word  ( new word == $brokenText[$i] )
-				$dimensions = $this->getFontPlacementCoordinates($temp, 10);
-				$this->PlaceTextOnImage(10, $textCoordinateX, $textHeight,$temp);
-				$finalOutput = $brokenText[$i];
-				$textHeight +=13;
-			}
-		
-			//if this is the last word append this also.The previous if will be true if the last word will have no room
-			if ($i == count($brokenText) - 1) {
-				$dimensions = $this->getFontPlacementCoordinates($finalOutput, 10);
-				$this->PlaceTextOnImage(10, $textCoordinateX, $textHeight,$finalOutput);
+			if ($textHeight <= $this->textMaxHeight){
+				$minFontSize = $fontSize;
+			}else{
+				$maxFontSize = $fontSize;
 			}
 		}
-		return $finalOutput;
+		$multipleLinesText = $this->returnMultipleLinesText($text,$minFontSize);
+		$this->placeTextOnImage($fontSize,$multipleLinesText,$type);
 	}
 
-	public function processImg()
-	{
-		if($this->bottomText != "") {
-		  $this->WorkOnImage($this->bottomText,30,"bottom");
+	private function returnMultipleLinesText($text, $fontSize){
+		$brokenText = explode(' ', $text);
+		$linesArray = array();
+		$tmpString = '';
+		$textHeight = 0;
+		foreach ($brokenText as $word){
+			$textBox = $this->getFontPlacementCoordinates($tmpString.' '.$word, $fontSize);
+			$lineWidth = abs($textBox[4] - $textBox[0]);
+			if($lineWidth  > $this->textMaxWidth){
+				$textBox = $this->getFontPlacementCoordinates($tmpString, $fontSize);
+				$lineHeight = abs($textBox[5] - $textBox[1]);
+				$linesArray[] = array($tmpString,$lineHeight);
+				$textHeight += $lineHeight;
+				$tmpString = $word;
+			}
+			else{
+				$tmpString.=' '.$word;
+			}
 		}
-		if($this->topText != "") {
-		  $this->WorkOnImage($this->topText,30,"top");
+		$textBox = $this->getFontPlacementCoordinates($tmpString, $fontSize);
+		$lineHeight = abs($textBox[5] - $textBox[1]);
+		$linesArray[] = array($tmpString,$lineHeight);
+		$textHeight += $lineHeight;
+		return array($linesArray, $textHeight);
+	}
+
+	private function processImg()
+	{
+		$this->textMaxWidth = round($this->width*1.1);
+		$this->textMaxHeight = round($this->height * 0.2);
+
+		if($this->bottomText != '') {
+		  $this->workOnImage($this->bottomText, 'bottom');
+		}
+		if($this->topText != '') {
+		  $this->workOnImage($this->topText, 'top');
 		}
 	}
 }
